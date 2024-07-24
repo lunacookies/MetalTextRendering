238fb87b9e01b958834ce61c5699026e220146c5
Luna Razzaghipour <luna@xoria.org>
2024-07-24 12:41:18 +1000

    Render text in `CGView`

--- Source/CGView.m
+++ Source/CGView.m
@@ -2,7 +2,14 @@
 
 - (void)drawRect:(NSRect)dirtyRect
 {
-	NSRectFill(NSMakeRect(0, 0, 50, 50));
+	NSAttributedString *attributedString = [[NSAttributedString alloc]
+	        initWithString:@"The quick brown fox jumps over the lazy dog."
+	            attributes:@{
+		            NSFontAttributeName : [NSFont fontWithName:@"Zapfino" size:50],
+		            NSForegroundColorAttributeName : NSColor.labelColor
+	            }];
+
+	[attributedString drawInRect:self.frame];
 }
 
 @end
